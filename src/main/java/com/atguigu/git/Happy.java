6b63bf3549bfa067a23a4e0bd45b3ef70a0a2290
eclipse_user <eclipse_user@atguigu.com>
2020-11-20 18:27:38 +0800

    edit for testing hot fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Happy {
  public static void main() {
 	 System.out.println("Happy file in TestGit ....");
+	 System.out.println("Happy file in hotfix ....");
  }
 }
